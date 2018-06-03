4d7a8bb56d73945a5ac1a401d6083de8df2dbc5c
vahriin <systemd219@gmail.com>
2018-06-03 23:40:45 +0300

    Add command-line arguments parsing and delete some code

--- server.py
+++ server.py
@@ -1,16 +1,26 @@
 import asyncio
 import json
 from itertools import cycle
+import argparse
+import sys
 
 import user
 import protocol
 
 class ChatServer:
     def __init__(self, host, port):
-        self._let_connecting = True
+        """Chat room for clients. Attributes:
+        - host: ip address of server
+        - port: port for connection
+        """
+
+        # Connected clients. Key is nick, value is User object
         self._clients = {}
+        # Delivering messages. The message will be removed whet it is delivered to all clients 
         self._messages = {}
+        # Cycle for sequential number of messages
         self._cycle = cycle([x for x in range(1000)])
+
         self._loop = asyncio.get_event_loop()
         self._server = self._loop.run_until_complete(
             asyncio.start_server(self.accept_connection, host=host, port=port)
@@ -18,17 +28,12 @@ class ChatServer:
         self._loop.run_forever()
 
     async def accept_connection(self, reader, writer):
-        print("conn accepted!")
-        if self._let_connecting:
-            writer.write(protocol.new_status().encode("utf8"))
-            writer.write(b'\n')
-            try:
-                await self.register_client(reader, writer)
-            except protocol.ProtocolException as pe:
-                writer.write(protocol.new_status(418, str(pe)).encode("utf8"))
-                writer.write(b'\n')
-        else:
-            writer.write(protocol.new_status(403).encode("utf8"))
+        writer.write(protocol.new_status().encode("utf8"))
+        writer.write(b'\n')
+        try:
+            await self.register_client(reader, writer)
+        except protocol.ProtocolException as pe:
+            writer.write(protocol.new_status(418, str(pe)).encode("utf8"))
             writer.write(b'\n')
 
     async def register_client(self, reader, writer):
@@ -36,26 +41,26 @@ class ChatServer:
         try:
             while True:
                 client = await self.get_client(reader, writer)
-                self._clients[client.nick]
+                self._clients[client.nick] # check this nick in clients dict
                 writer.write(protocol.new_status(409).encode("utf8"))
                 writer.write(b'\n')
         except KeyError:
             self._clients[client.nick] = client
-            print("user {} connected".format(str(client.nick)))
+            print("User {} connected.".format(str(client.nick)))
             writer.write(protocol.new_status().encode("utf8"))
             writer.write(b'\n')
             await self.client_handler(client)
 
     async def get_client(self, reader, writer):
         user_data = (await reader.readline()).decode("utf8")
-        _type, user_data = protocol.load(user_data)
-        client = user.User(user_data, reader, writer)
+        _type, data = protocol.load(user_data)
+        client = user.User(data, reader, writer)
         return client
 
     async def client_handler(self, client):
         while True:
             message_type, message = await client.read_message()
-            if message_type == None:
+            if message_type == None: #connection is broken
                 return
             elif message_type: # message_type is "message"
                 self.message_handler(message, client)
@@ -65,7 +70,6 @@ class ChatServer:
     def message_handler(self, message, client):
         message_id = next(self._cycle)
         self._messages[message_id] = 0
-        #client.send_raw(protocol.new_status_message(message_id))
         message["messageId"] = message_id
         self.broadcast(message, client.nick)
 
@@ -82,7 +86,12 @@ class ChatServer:
             client.send_raw(protocol.new_status_message(message["messageId"]))
 
         
-
+def new_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", default='59503')
+    return parser
 
 if __name__ == '__main__':
-    server = ChatServer('127.0.0.1', 2805)
+    parser = new_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    server = ChatServer('', namespace.port)
